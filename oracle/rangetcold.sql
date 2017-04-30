6770f35f75b356f31346354194fefa7bcfc6c547
Luís Marques <lcarapinha@gmail.com>
2017-04-30 21:51:32 +0100

    partition fixes

--- oracle/rangetcold.sql
+++ oracle/rangetcold.sql
@@ -4,7 +4,7 @@ where pt.owner not in ('SYS', 'SYSTEM', 'AUDSYS')
 and pt.owner = pp.table_owner
 and pt.partitioning_type = 'RANGE'
 )
-select to_char(END_INTERVAL_TIME, 'mon-yyyy') as DATE_INTERVAL, rt.table_owner, rt.table_name, rt.partition_name, object_type, max(db_block_changes_delta) BLOCK_CHANGES, max(physical_writes_delta) PHYSICAL_WRITES
+select rt.table_owner, rt.table_name, rt.partition_name, object_type, max(db_block_changes_delta) BLOCK_CHANGES, max(physical_writes_delta) PHYSICAL_WRITES
 from dba_hist_seg_stat ss, dba_objects dbo, range_partitioned rt, DBA_HIST_SNAPSHOT dbh
 where rt.table_name = dbo.object_name
 and dbo.object_id = ss.obj#
@@ -12,4 +12,4 @@ and dbh.snap_id = ss.snap_id
 --3 months
 and END_INTERVAL_TIME >= sysdate - 90
 group by to_char(END_INTERVAL_TIME, 'mon-yyyy'), rt.table_owner, rt.table_name, rt.partition_name, object_type
-order by 1,2,3,4,5,6 desc
+order by BLOCK_CHANGES asc, PHYSICAL_WRITES asc
